04cf403dd59392ffd1c86b1a66ee6ac7a066371a
HisamNasir <hisamnasir1999@gmail.com>
2023-01-19 17:58:22 +0500

    Routes working

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,12 +1,23 @@
-
+import { Route, Routes } from "react-router-dom";
+import {Home} from "./pages/Home/index"
+import {About} from "./pages/About/index"
+import {Approvals} from "./pages/Approvals/index"
+import {Blog} from "./pages/Blog/index"
+import {Careers} from "./pages/Careers/index"
+import {Contact} from "./pages/Contact/index"
+import {Projects} from "./pages/Projects/index"
+import {Services} from "./pages/Services/index"
 function App() {
-  return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
-    </>
-  );
+  return <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/About" element={<About/>}/>
+    <Route path="/Approvals" element={<Approvals/>}/>
+    <Route path="/Blog" element={<Blog/>}/>
+    <Route path="/Careers" element={<Careers/>}/>
+    <Route path="/Contact" element={<Contact/>}/>
+    <Route path="/Projects" element={<Projects/>}/>
+    <Route path="/Services" element={<Services/>}/>
+  </Routes>
 }
 
 export default App;
--- my-project/src/pages/Approvaals/index.js
+++
@@ -1,3 +0,0 @@
-export function Aproval(){
-    return <h1>Aproval</h1>
-}
